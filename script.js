d7ca1094ad8b4c4c99c1f06e91ec56f958fc1f91
Yukihan <yukanzen@hotmail.com>
2023-05-06 07:50:03 -0300

    Counter v1 Finished

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-=======
-var data = new Date();
-
->>>>>>> ac6b1a50c8bcc46256bdfe5a5ff44ff5be7e32b5
 var elemCentiseconds = document.getElementById('centiseconds');
 var elemSeconds = document.getElementById('seconds');
 var elemMinutes = document.getElementById('minutes');
 var elemHours = document.getElementById('hours');
 var startCounter = document.getElementById('start');
 var stopCounter = document.getElementById('stop');
-<<<<<<< HEAD
 var resetCounter = document.getElementById('reset');
 var interval;
 var timeRecover = 0;
@@ -31,23 +25,6 @@ startCounter.onclick = function() {
 function count() {
     timeNow1 = Date.now() - time1;
     //timeNow1 *= 1; //speed up for tests
-=======
-var interval;
-
-var time1 = 0;
-startCounter.onclick = function() {
-    time1 = Date.now();
-    interval = setInterval(count, 16);
-}
-
-stopCounter.onclick = function () {
-    
-}
-
-function count() {
-    var timeNow1 = Date.now() - time1;
-    timeNow1 *= 60; //speed up for tests
->>>>>>> ac6b1a50c8bcc46256bdfe5a5ff44ff5be7e32b5
     var seconds = Math.floor(timeNow1/1000)
     seconds = seconds - Math.floor(timeNow1/60000)*60
 
@@ -60,31 +37,19 @@ function count() {
 
 
     if (hours < 10) {
-<<<<<<< HEAD
         elemHours.innerHTML = '0' + hours;
-=======
-        elemHours.innerHTML = "0" + hours;
->>>>>>> ac6b1a50c8bcc46256bdfe5a5ff44ff5be7e32b5
     } else {
         elemHours.innerHTML = hours;
     }
     
     if (minutes < 10) {
-<<<<<<< HEAD
         elemMinutes.innerHTML = '0' + minutes;
-=======
-        elemMinutes.innerHTML = "0" + minutes;
->>>>>>> ac6b1a50c8bcc46256bdfe5a5ff44ff5be7e32b5
     } else {
         elemMinutes.innerHTML = minutes;
     }
 
     if (seconds < 10) {
-<<<<<<< HEAD
         elemSeconds.innerHTML = '0' + seconds;
-=======
-        elemSeconds.innerHTML = "0" + seconds;
->>>>>>> ac6b1a50c8bcc46256bdfe5a5ff44ff5be7e32b5
     } else {
         elemSeconds.innerHTML = seconds;
     }
@@ -92,7 +57,6 @@ function count() {
     elemCentiseconds.innerHTML = centiseconds;
 }
 
-<<<<<<< HEAD
 stopCounter.onclick = function () {
     interval = clearInterval(interval);
     timeRecover = Date.now();
@@ -107,31 +71,3 @@ resetCounter.onclick = function() {
     elemCentiseconds.innerHTML = '00'
     timeRecover = 0
 }
-=======
-/*var seconds = 0;
-var centiseconds = 0;
-var interval = 0;
-var elemSeconds = document.getElementById('seconds');
-var elemCentiseconds = document.getElementById('centiseconds');
-var startCounter = document.getElementById('start');
-var stopCounter = document.getElementById('stop');
-
-startCounter.onclick = function() {
-    interval = setInterval(count, 10);
-}
-
-stopCounter.onclick = function() {
-    clearInterval(interval);
-    startCounter.innerHTML = 'RESUME';
-}
-
-function count() {
-    centiseconds += 1;
-    if (centiseconds > 99) {
-        seconds += 1;
-        centiseconds = 0;
-    }
-    elemSeconds.innerHTML = seconds;
-    elemCentiseconds.innerHTML = centiseconds;
-}*/
->>>>>>> ac6b1a50c8bcc46256bdfe5a5ff44ff5be7e32b5
